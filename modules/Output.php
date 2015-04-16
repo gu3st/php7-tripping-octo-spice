9c0798cd686859a81ea686c628215a7554998d18
gu3st <gu3st@dustindawes.com>
2015-04-16 16:25:27 -0700

    I've gone mad

--- modules/Output.php
+++ modules/Output.php
@@ -1,18 +1,27 @@
 <?php
-$x = class{
+return function($arguments) {
+    return call_user_func_array(class {
 
-    public function Out($value){
-        echo $value;
-    }
+        public function __construct($a = "a",$b = "b"){
+            echo "A : " . $a;
+            echo "B : " .$b;
+        }
 
-    public function Shout($value){
-        echo strtoupper($value);
-    }
+        public function Out($value)
+        {
+            echo $value;
+        }
 
-    public function Whisper($value){
-        echo strtolower($value);
-    }
+        public function Shout($value)
+        {
+            echo strtoupper($value);
+        }
 
+        public function Whisper($value)
+        {
+            echo strtolower($value);
+        }
+
+
+    }, func_get_args());
 };
-
-return $x;
